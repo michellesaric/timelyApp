5bc4dee468c1931fdb33f0e20de96de5aa48af6d
„michellesaric“ <msaric03@fesb.hr>
2022-12-17 08:32:22 +0100

    Modified appsettings.json and added migrations

--- TimelyApp/TimelyApp.Data/Entities/TimelyAppContext.cs
+++ TimelyApp/TimelyApp.Data/Entities/TimelyAppContext.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimelyApp.Data.Entities.Models;
 
 namespace TimelyApp.Data.Entities
 {
-    internal class TimelyAppDbContext
+    public class TimelyAppContext : DbContext
     {
+        public TimelyAppContext(DbContextOptions<TimelyAppContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ProjectTime> ProjectTimes { get; set; }
     }
 }
