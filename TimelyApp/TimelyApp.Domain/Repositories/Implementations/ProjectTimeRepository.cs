81b99b12ff1688dc6af30a3e86a43432ff66cfce
„michellesaric“ <msaric03@fesb.hr>
2022-12-17 08:47:51 +0100

    Added ProjectTimeRepository class for all function logic, added pagination logic

---
+++ TimelyApp/TimelyApp.Domain/Repositories/Implementations/ProjectTimeRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimelyApp.Data.Entities;
+using TimelyApp.Data.Entities.Models;
+using TimelyApp.Domain.Repositories.Interfaces;
+
+namespace TimelyApp.Domain.Repositories.Implementations
+{
+    internal class ProjectTimeRepository : IProjectTimeRepository
+    {
+        private readonly TimelyAppContext _timelyAppContext;
+
+        public ProjectTimeRepository(TimelyAppContext timelyContext)
+        {
+            _timelyAppContext = timelyContext;
+        }
+
+        public List<ProjectTime> Pagination(int pageNumber)
+        {
+            if (pageNumber < 1)
+            {
+                pageNumber = 1;
+            }
+            var projectTimes = _timelyAppContext
+                .ProjectTimes.Skip((pageNumber - 1) * 10)
+                .Take(10)
+                .ToList();
+
+            return projectTimes;
+        }
+
+    }
+}
